a6a407a02faeefd3b6c00f281df19f458e1d96df
BaabIran <BaabIran@DESKTOP-9QDIUPO>
2023-09-16 20:02:09 +0330

    Add project files.

---
+++ GlobalTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using GlobalTicket.TicketManagement.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface ICategoryRepository : IAsyncRepository<Category>
+    {
+
+    }
+}
---
+++ GlobalTicket.TicketManagement.Domain/Entites/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Domain.Entites
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
